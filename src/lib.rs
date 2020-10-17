1528ac74f94072a26a228ca635aa4a2ae12c6574
Aurelien Foucault <aurelien.foucault@epitech.eu>
2020-10-17 23:42:49 +0200

    Basic structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,45 @@
 use proc_macro::TokenStream;
 use quote::quote;
-#[proc_macro_derive(IntoDefault)]
+use syn::Ident;
+use syn::{parse_macro_input, DeriveInput};
+
+fn _print_type_of<T>(_: &T) {
+   println!("{}", std::any::type_name::<T>())
+}
+
+#[proc_macro_derive(IntoDefault, attributes(IntoStruct))]
 pub fn into_default(item: TokenStream) -> TokenStream {
-   let code = quote! {};
-   TokenStream::from(code)
+    let ast = parse_macro_input!(item as DeriveInput);
+    let attributes = &ast.attrs;
+    let base_type = &ast.ident;
+    let mut code: TokenStream = TokenStream::from(quote! {});
+    //println!("{:#?}", &ast);
+    for attribute in attributes {
+        if attribute.path.is_ident("IntoStruct") {
+            let type_struct = attribute.parse_args::<Ident>().unwrap();
+            if let syn::Data::Struct(d) = &ast.data {
+               if let syn::Fields::Named(f) = &d.fields {
+                  let fields = &f.named;
+                  let mut fields_name = Vec::new();
+                  //println!("{:#?}", &fields);
+                  for field in fields {
+                     fields_name.push(&field.ident)
+                  }
+                  //print_type_of(&fields);
+                  code = TokenStream::from(quote! {
+                     impl From<#base_type> for #type_struct {
+                        fn from(item: #base_type) -> Self {
+                           #type_struct {
+                              #(#fields_name: item.#fields_name,)*
+                           }
+                        }
+                     }
+                  });
+            }
+            }
+        } else {
+            TokenStream::from(quote! {});
+        }
+    }
+    TokenStream::from(code)
 }
--- tests/basic_struct.rs
+++ tests/basic_struct.rs
@@ -2,21 +2,29 @@ use default_conversion::*;
 
 #[test]
 fn basic_struct() {
+    #[derive(Debug, PartialEq, Eq)]
     struct A {
         a: String,
-        b: i32
+        b: i32,
     };
 
-    #[derive(IntoDefault)]
+    #[derive(Debug, PartialEq, Eq, IntoDefault)]
+    #[IntoStruct(A)]
     struct InputA {
         a: String,
-        b: i32
+        b: i32,
     };
 
     let a = InputA {
         a: String::from("test"),
-        b: 2
+        b: 2,
     };
 
-    assert_eq!(2, 2);
+    assert_eq!(
+        A {
+            a: String::from("test"),
+            b: 2
+        },
+        A::from(a)
+    );
 }
